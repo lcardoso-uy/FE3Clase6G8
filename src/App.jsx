1589f40f502293aadda597e0c6098b22aed9766e
lcardoso-uy <lcardosogx@gmail.com>
2023-08-10 22:15:59 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Form from './Form'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Registre su pokemon</h1>  
+      <Form />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Form(){
+
+    const[nombre,setNombre] = useState("")
+    const[edad,setEdad] = useState("")
+    const[pokemon,setPokemon] = useState("")
+
+    function handleNombre(e){
+        setNombre(e.target.value)
+    }
+
+    function handleEdad(e){
+        setEdad(e.target.value)
+    }
+
+    function handlePokemon(e){
+        setPokemon(e.target.value)
+    }
+
+    function registrar(e){
+        e.preventDefault();
+        console.log("Usuario registrado");
+        console.log(nombre);
+        console.log(edad);
+        console.log(pokemon);
+    }
+
+    
+    return(
+        <div>
+            <h2>Información del usuario</h2>
+            <form onSubmit={registrar}>
+                <div>
+                    <input type="text" placeholder="Ingrese su nombre" onChange={handleNombre}/>
+                    <input type="text" placeholder="Ingrese su edad" onChange={handleEdad}/>
+                    <input type="text" placeholder="Ingrese su pokemon favorito" onChange={handlePokemon}/>
+                    <input type="submit" value="registrar" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
